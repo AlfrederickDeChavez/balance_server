ecb09eeb7c16917cc31ffb7dbef29797c3270f34
AlfrederickDeChavez <dechavezalfrederick@gmail.com>
2022-11-01 08:25:01 +0800

    Update quantity field

--- foods/models.py
+++ foods/models.py
@@ -7,7 +7,7 @@ User = get_user_model()
 class Food(models.Model):
     name = models.CharField(max_length=255, blank=True)
     category = models.CharField(max_length=255, blank=True)
-    quantity = models.IntegerField()
+    quantity = models.FloatField()
     date_consumed = models.DateTimeField()
     user = models.ManyToManyField(User)
     calories = models.FloatField(null=True, blank=True)
